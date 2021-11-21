6c64537ff69d7557e8e7fc68498ac6c060c9ca08
ZeusenAres <2060537@talnet.nl>
2021-11-21 11:03:22 +0100

    changed a little shit

---
+++ Beroepsproject Rent-A-Car/Beroepsproject Rent-A-Car/IMG/Beroepsproject Rent-A-Car/Beroepsproject Rent-A-Car/Homepage/Homepage.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="Homepage.css">
+       
+        <title>Homepage</title>
+    </head>
+    <body>
+
+        <nav>
+            <div class="navbar">
+                <img id="logo-navbar" src="../IMG/Logo-2.png" alt="[LOGO]">
+                <div class="navbar-links">
+                <a href="">Reserveer hier</a>
+                <a href="">Contact</a>
+                <a href="">Inloggen</a>
+                </div>
+            </div>
+        </nav>
+
+        <div class="homepage-body">
+        <button id="Reserveer-knop" type="button">Reserveer nu!!</button>
+        <img id="homepage-pic" src="../IMG/multicars-juist.jpg">
+        </div>
+
+    </body>
+</html>
