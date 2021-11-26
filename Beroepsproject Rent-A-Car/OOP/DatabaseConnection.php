c4e3e79968db62205f3eaf0b1a75452a207d2fb4
ZeusenAres <2060537@talnet.nl>
2021-11-26 10:25:54 +0100

    added oop

---
+++ Beroepsproject Rent-A-Car/OOP/DatabaseConnection.php
@@ -0,0 +1,34 @@
+<?php
+class DatabaseConnection
+{
+    private string $host;
+    private string $dbname;
+    private string $user;
+    private string $pass;
+
+    public function __construct(string $host, string $dbname, string $user, string $pass)
+    {
+        $this->host = $host;
+        $this->dbname = $dbname;
+        $this->user = $user;
+        $this->pass = $pass;
+    }
+
+    public function connect()
+    {
+        try
+        {
+            $dsn = "mysql:host=$this->host;dbname=$this->dbname";
+            $conn = new PDO($dsn, $this->user, $this->pass);
+
+            if(!$conn)
+            {
+                throw new PDOException("Failed to connect");
+            }
+        }
+        catch (PDOException $ex)
+        {
+            echo $ex->getMessage();
+        }
+    }
+}
